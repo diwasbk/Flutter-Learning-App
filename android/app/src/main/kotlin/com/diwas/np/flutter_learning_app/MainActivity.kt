18fb9e75d6aa1656870abfb1c4279b834a09a076
diwasbk <diwasbk903@gmail.com>
2025-12-14 19:33:06 +0545

    App Setup with MaterialApp and Scaffold

---
+++ android/app/src/main/kotlin/com/diwas/np/flutter_learning_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.diwas.np.flutter_learning_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learning_app/screens/first_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstScreen(),
+    );
+  }
+}
