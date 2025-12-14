d11f754bb9cde1ee5f9f5f6c7c4dcb22b5ab619d
diwasbk <diwasbk903@gmail.com>
2025-12-15 00:14:09 +0545

    Local and Network Image

--- lib/screens/image_screen.dart
+++ lib/screens/image_screen.dart
@@ -14,6 +14,27 @@ class ImageScreen extends StatelessWidget {
         ),
         actions: [Icon(Icons.search, color: Colors.white)],
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 500,
+              height: 300,
+              child: Image.asset("assets/images/nepal.jpg", fit: BoxFit.fill),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: 500,
+              height: 300,
+              child: Image.network(
+                "https://th.bing.com/th/id/R.78904fa22eef70acfc8ed53647ccfe66?rik=a%2fq%2bKKpNaW8LBw&riu=http%3a%2f%2fglobaltravelservicesonline.com%2fwp-content%2fuploads%2f2013%2f06%2fMount-Everest-Gold-summit.jpg&ehk=SoIwDq15xkP5ubCmEjL7Zm367dNCproBW50epXmKPE8%3d&risl=1&pid=ImgRaw&r=0",
+                fit: BoxFit.fill,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
