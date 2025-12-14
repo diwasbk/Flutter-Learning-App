c756d338c867af1e00519c4780109832d650ba4d
diwasbk <diwasbk903@gmail.com>
2025-12-14 19:52:56 +0545

    Body with Center Container and Text

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -8,13 +8,31 @@ class FirstScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
-        title: Text("WhatsApp", style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold
-        ),),
-        actions: [
-          Icon(Icons.search, color: Colors.white,)
-        ],
+        title: Text(
+          "WhatsApp",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        actions: [Icon(Icons.search, color: Colors.white)],
+      ),
+
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.lightBlueAccent,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Center(
+            child: Text(
+              "Welcome here!",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
