e420236ecb7d98798c4e28d88deb5ca6f1c3958d
diwasbk <diwasbk903@gmail.com>
2025-12-23 17:38:43 +0545

    Student Model

---
+++ lib/models/student_model.dart
@@ -0,0 +1,11 @@
+class StudentModel{
+  final String fName;
+  final String lName;
+  final String dob;
+
+  StudentModel({
+    required this.fName,
+    required this.lName,
+    required this.dob
+  });
+}
